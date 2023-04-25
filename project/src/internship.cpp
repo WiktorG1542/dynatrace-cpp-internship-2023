243c7b40e781a20ee803e99f79347a47d6562126
wiktor <w.grzywacz.809@studms.ug.edu.pl>
2023-04-25 23:00:00 +0200

    my solution

--- project/src/internship.cpp
+++ project/src/internship.cpp
@@ -18,93 +18,91 @@ struct OS {
 
 //this function is used to sort the vector of operating systems
 bool compareOs(OS &os1, OS &os2) {
-    if (os1.days>os2.days) {
-        return true;
-    } else {
-        return false;
-    }
+    return (os1.days > os2.days);
 }
 
 namespace internship {
 
-    std::vector<OS> mySolution(const std::string& jsonFileName, int elementsCount) {
-        
+    void mySolution(const std::string &jsonFileName, int elementsCount) {
+
         std::ifstream f(jsonFileName);
         json data = json::parse(f);
 
-        std::vector<OS> allOperatingSystems;
+        std::vector <OS> allOperatingSystems;
 
         for (size_t i = 0; i < data.size(); i++) {
 
-            if (data[i]["os"]==true) {
+            if (data[i]["os"] == true) {
 
-                for (size_t versionIterator = 0; versionIterator<data[i]["versions"].size(); versionIterator++) {
+                for (size_t versionIterator = 0; versionIterator < data[i]["versions"].size(); versionIterator++) {
 
-                    if (data[i]["versions"][versionIterator].contains("releaseDate") && data[i]["versions"][versionIterator].contains("eol") && data[i]["versions"][versionIterator]["releaseDate"].is_string() && data[i]["versions"][versionIterator]["eol"].is_string()) {
+                    json currentVersion = data[i]["versions"][versionIterator];
 
-                        std::string date1 = data[i]["versions"][versionIterator]["releaseDate"];
-                        std::string date2 = data[i]["versions"][versionIterator]["eol"];
+                    if (currentVersion.contains("releaseDate") &&
+                        currentVersion.contains("eol") &&
+                        currentVersion["releaseDate"].is_string() &&
+                        currentVersion["eol"].is_string()) {
+
+                        //Now we have included in our search only the versions which are an OS,
+                        //have "eol" and "releaseDate" fields which are both strings. 
+                        //Any other objects which do not meet those requirements will not be included in the answer.
                         
-                        std::istringstream is1(date1);
-                        std::istringstream is2(date2);
-                        
-                        date::sys_days d1, d2;
-                        is1 >> date::parse("%F", d1);
-                        is2 >> date::parse("%F", d2);
+                        std::string releaseDateString = currentVersion["releaseDate"];
+                        std::string eolDateString = currentVersion["eol"];
 
-                        auto diff = d2 - d1;
+                        std::istringstream is1(releaseDateString);
+                        std::istringstream is2(eolDateString);
+
+                        date::sys_days releaseDate, eolDate;
+                        is1 >> date::parse("%F", releaseDate);
+                        is2 >> date::parse("%F", eolDate);
+
+                        auto diff = eolDate - releaseDate;
                         int days = diff.count();
 
                         OS currentOs;
                         currentOs.name = data[i]["name"];
-                        currentOs.cycle = data[i]["versions"][versionIterator]["cycle"];
+                        currentOs.cycle = currentVersion["cycle"];
                         currentOs.days = days;
 
                         allOperatingSystems.push_back(currentOs);
-                    
-                    } else {
-                        //if the program reaches this bit, it means a version either has missing "releaseDate" or "eol",
-                        //or the "eol" or "releaseDate" is not a string. In this case, we can just disregard that particular object.
+
                     }
 
                 }
-
             }
         }
 
         std::sort(allOperatingSystems.begin(), allOperatingSystems.end(), compareOs);
 
-        if (elementsCount>allOperatingSystems.size()) {
-            std::cout << "WARNING! You are trying to get " << elementsCount << " operating systems, but there are only " << allOperatingSystems.size() << "\n";
+        // I have decided to add this, because in the task description it was not specified what to do when for example we want to
+        // print info about a 100 operating systems, but there are only 5. Since in that case we can only print as many as there are,
+        // I have decided to add this warning so that the user is notified in this edge case
+        if (elementsCount > allOperatingSystems.size()) {
+            std::cout << "WARNING! You are trying to get " << elementsCount << " operating systems, but there are only "
+                      << allOperatingSystems.size() << "\n";
             std::cout << "proceeding to print out all of the operating systems\n";
         }
 
-        int howManyDoWePrint;
+        int howManyDoWePrint = (elementsCount > allOperatingSystems.size()) ? allOperatingSystems.size() : elementsCount;
 
-        if (elementsCount>allOperatingSystems.size()) {
+        if (elementsCount > allOperatingSystems.size()) {
             howManyDoWePrint = allOperatingSystems.size();
         } else {
             howManyDoWePrint = elementsCount;
         }
 
-        for (int a=0; a<howManyDoWePrint; a++) {
+        for (int i = 0; i < howManyDoWePrint; i++) {
             //we have to add one to the number of days, since we are including the day of the eol date as well
-            std::cout << allOperatingSystems[a].name << " " << allOperatingSystems[a].cycle << " " << allOperatingSystems[a].days+1 << "\n";
+            std::cout << allOperatingSystems[i].name << " " << allOperatingSystems[i].cycle << " "
+                      << allOperatingSystems[i].days + 1 << "\n";
         }
-
-        //make sure that the vector we are returning for tests is the right size
-        while (allOperatingSystems.size()>elementsCount) {
-            allOperatingSystems.pop_back();
-        }
-
-        return allOperatingSystems;
     }
 
     // do not remove this function
-    void solution(const std::string& jsonFileName, int elementsCount) {
+    void solution(const std::string &jsonFileName, int elementsCount) {
 
         // put the call to your solution here
         mySolution(jsonFileName, elementsCount);
-
     }
 }
